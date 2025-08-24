5c325c3b1a3fd59a565abe0ef18691c8ad3ddcfe
hellochris06 <hellochris06@naver.com>
2025-08-25 05:43:24 +0900

    Delete problem-bank directory

--- problem-bank/app/admin/problems/[id]/page.tsx
+++
@@ -1,6 +0,0 @@
-
-import EditProblemForm from '@/components/admin/EditProblemForm';
-
-export default function EditProblemPage({ params }: { params: { id: string } }) {
-  return <div className="card"><EditProblemForm id={params.id} /></div>;
-}
--- problem-bank/app/admin/problems/new/page.tsx
+++
@@ -1,6 +0,0 @@
-
-import NewProblemForm from '@/components/admin/NewProblemForm';
-
-export default function NewProblemPage() {
-  return <div className="card"><NewProblemForm /></div>;
-}
